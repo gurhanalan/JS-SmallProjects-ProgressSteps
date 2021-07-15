86c429f741a91267a958e81bf3d3ceedb3046431
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-07-15 21:55:14 +0300

    Section 1 add js and style

---
+++ js/script1.js
@@ -0,0 +1,38 @@
+// Selections
+
+const btnPrev = document.querySelector(".btn-prev");
+const btnNext = document.querySelector(".btn-next");
+
+let step = 0;
+
+function btnDisable() {
+    if (step >= 3) {
+        btnNext.disabled = true;
+        btnNext.classList.add("btn--disabled");
+    } else if (step <= 0) {
+        btnPrev.disabled = true;
+        btnPrev.classList.add("btn--disabled");
+    } else {
+        btnNext.disabled = false;
+        btnPrev.disabled = false;
+
+        btnPrev.classList.remove("btn--disabled");
+        btnNext.classList.remove("btn--disabled");
+    }
+}
+
+btnNext.addEventListener("click", () => {
+    step++;
+    document.querySelector(`.bridge-${step}`).classList.add("bridge--active");
+    document.querySelector(`.step-${step}`).classList.add("step--active");
+    btnDisable();
+});
+
+btnPrev.addEventListener("click", () => {
+    document
+        .querySelector(`.bridge-${step}`)
+        .classList.remove("bridge--active");
+    document.querySelector(`.step-${step}`).classList.remove("step--active");
+    step--;
+    btnDisable();
+});
