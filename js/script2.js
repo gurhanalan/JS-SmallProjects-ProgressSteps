1627d07e86743f3bca730be26aeeb73232400e6a
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-07-16 06:27:12 +0300

    Add section 3 files and markup

--- js/script2.js
+++ js/script2.js
@@ -5,4 +5,11 @@ const inputSearch = document.querySelector(".search-input");
 btnSearch.addEventListener("click", () => {
     inputSearch.classList.toggle("search-input--active");
     btnSearch.classList.toggle("search-btn--active");
+    inputSearch.focus();
+
+    // if (inputSearch.classList.contains("search-input--active")) {
+    //     inputSearch.focus();
+    // } else {
+    //     btnSearch.focus();
+    // }
 });
