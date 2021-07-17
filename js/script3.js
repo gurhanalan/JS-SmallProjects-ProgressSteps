41f3320f2385075e95d8b4534e71ed415fbc2ec1
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-07-17 07:12:01 +0300

    Add section 3 style and JS 2

--- js/script3.js
+++ js/script3.js
@@ -2,6 +2,11 @@ const pages = document.querySelector(".pages");
 const page1 = document.querySelector(".page--1");
 const page2 = document.querySelector(".page--2");
 
-page1.addEventListener("click", () => {
-    pages.classList.toggle("pages--left");
+page1.addEventListener("mouseover", () => {
+    pages.classList.add("pages--left");
+    pages.classList.remove("pages--right");
+});
+page2.addEventListener("mouseover", () => {
+    pages.classList.remove("pages--left");
+    pages.classList.add("pages--right");
 });
